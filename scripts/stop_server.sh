3a597e20c11f70ef09fa9859ed1ecdac5c8e5988
jssdevops <129446350+jssdevops@users.noreply.github.com>
2023-09-27 15:44:13 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-if ! [ -x "$(command -v nginx)" ]; then
-  sudo apt update
-  sudo apt install -y nginx
-  exit 1
-fi
-
-# Nginx is already installed
-echo "Nginx is already installed."
-
+#!/bin/bash 
+systemctl stop nginx
